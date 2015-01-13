0fc839d3d4235d0573337ed485056431e201beae
joefroh <jmafrohlinger@outlook.com>
2015-01-12 21:34:17 -0800

    deserializing to the model works correctly, brought in Json.Net to do it

--- GitHubby/WebFetcher.cs
+++ GitHubby/WebFetcher.cs
@@ -1,34 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
+using GitHubby.Models;
+using Newtonsoft.Json;
 
 namespace GitHubby
 {
     public class WebFetcher
     {
-
-        public WebFetcher()
-        {
-
-        }
-
         public void Fetch()
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress= new Uri("https://api.github.com");
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.github.com");
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "TestApp");
-            
-            HttpResponseMessage response = client.GetAsync("").Result;
-            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
 
+            HttpResponseMessage response = client.GetAsync("users/joefroh/repos").Result;
+            var repoJson = response.Content.ReadAsStringAsync().Result;
+            var data = JsonConvert.DeserializeObject<Repo[]>(repoJson);
         }
     }
 }
