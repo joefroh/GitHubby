cda59c36a6870ae757c9cdccbf793df9dda1a0de
Joey Frohlinger <jmafrohlinger@outlook.com>
2015-01-12 16:01:48 -0800

    Starting to add some form, nothing of note yet

---
+++ GitHubby/WebFetcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubby
+{
+    public class WebFetcher
+    {
+
+        public WebFetcher()
+        {
+            
+        }
+
+        public void Fetch()
+        {
+            using (var client = new HttpClient())
+            {
+               
+            }
+
+        }
+    }
+}
